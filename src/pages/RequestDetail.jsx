0716296b112ce831d259e9a6f6e085f0cb562e60
DingX2 <pourding@naver.com>
2023-09-15 03:47:17 +0900

    [FEAT] Grid 구현 완료
    - NavBar 자연스럽게 수정완료
    - Grid hover active 추가

--- src/pages/RequestDetail.jsx
+++ src/pages/RequestDetail.jsx
@@ -1,22 +1,29 @@
 import { Profile } from "../components/Profile";
 import "./RequestDetail.scss";
-
+import Grid from "../components/Grid";
 
 export default function RequestDetail() {
-    return (
-        <Profile.Wrapper>
-            <Profile.Aside imgSrc={""} igLink={"http://www.naver.com"} fbLink={"http://www.google.com"}></Profile.Aside>
-            <Profile.Content
-                title="My Profile"
-                rating={5}
-                theme={["테마", "긴 테마"]}
-                region="대구광역시 수성구 범어동"
-                time="1시간 ~ "
-                phone="010-1234-1234"
-                price="50,000원"
-                description="설명설명설명"
-            />
-        </Profile.Wrapper>
-    );
-}
+  return (
+    <div>
+      <Profile.Wrapper>
+        <Profile.Aside
+          imgSrc={""}
+          igLink={"http://www.naver.com"}
+          fbLink={"http://www.google.com"}
+        ></Profile.Aside>
+        <Profile.Content
+          title="My Profile"
+          rating={5}
+          theme={["테마", "긴 테마"]}
+          region="대구광역시 수성구 범어동"
+          time="1시간 ~ "
+          phone="010-1234-1234"
+          price="50,000원"
+          description="설명설명설명"
+        />
+      </Profile.Wrapper>
 
+      <Grid />
+    </div>
+  );
+}
