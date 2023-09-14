d8afc71223efeed71fd864909e98d4e58b82b477
toothlessdev <toothless042@gmail.com>
2023-09-15 02:47:18 +0900

    fix: Rating 컴포넌트number props 연결

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,36 +5,18 @@ import { faStar as rfaStar } from "@fortawesome/free-regular-svg-icons";
 
 import "./Rating.scss";
 
-export default function Rating(props) {
-  const number = 3;
-
-  function star(number) {
+export default function Rating({ number }) {
     return (
-      <div className="Rating__stars-container">
-        <div className="Rating__stars">
-          {[...Array(number)].map((_, index) => (
-            <FontAwesomeIcon className="star" key={index} icon={sfaStar} />
-          ))}
-          {[...Array(5 - number)].map((_, index) => (
-            <FontAwesomeIcon
-              className="star"
-              key={number + index}
-              icon={rfaStar}
-            />
-          ))}
+        <div className="Rating__stars-container">
+            <div className="Rating__stars">
+                {[...Array(number)].map((_, index) => (
+                    <FontAwesomeIcon className="star" key={index} icon={sfaStar} />
+                ))}
+                {[...Array(5 - number)].map((_, index) => (
+                    <FontAwesomeIcon className="star" key={number + index} icon={rfaStar} />
+                ))}
+            </div>
+            <span className="Rating__stars-text">{number}/5</span>
         </div>
-        <span className="Rating__stars-text">{number}/5</span>
-      </div>
     );
-  }
-
-  return (
-    <div className="Rating-wrapper">
-      <div className="Bstar">
-        <FontAwesomeIcon icon={sfaStar} />
-      </div>
-      <span className="Rating-text">별점</span>
-      {star(number)}
-    </div>
-  );
 }
