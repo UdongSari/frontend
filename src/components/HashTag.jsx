969083e0a63a7cba0f29fe869e0f819c1b3f301e
toothlessdev <toothless042@gmail.com>
2023-09-15 02:46:05 +0900

    fix: HashTag 좌우정렬해주는 Group 컴포넌트 구현

--- src/components/HashTag.jsx
+++ src/components/HashTag.jsx
@@ -1,17 +1,22 @@
 import { useLayoutEffect, useRef } from "react";
 import "./HashTag.scss";
 
-export const HashTag = () => {
-    const itemRef = useRef();
-    useLayoutEffect(() => {
-        if (active) itemRef.current.classList.add("hashtag-active");
-        else itemRef.current.classList.remove("hashtag-active");
-    }, [active]);
+export const HashTag = {
+    Group: ({ children }) => {
+        return <div className="hashtag-group">{children}</div>;
+    },
+    Item: ({ active, children, ...rest }) => {
+        const itemRef = useRef();
+        useLayoutEffect(() => {
+            if (active) itemRef.current.classList.add("hashtag-active");
+            else itemRef.current.classList.remove("hashtag-active");
+        }, [active]);
 
-    return (
-        <div ref={itemRef} className="hashtag-item" {...rest}>
-            <span># </span>
-            <span>{children}</span>
-        </div>
-    );
+        return (
+            <div ref={itemRef} className="hashtag-item" {...rest}>
+                <span># </span>
+                <span>{children}</span>
+            </div>
+        );
+    },
 };
