c09990e903dd82b9f46c8f472a37de5f04a88879
toothlessdev <toothless042@gmail.com>
2023-09-14 16:43:11 +0900

    style: global 스타일 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+
+import "./styles/globals.scss";
+
 import { BrowserRouter } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AnimatePresence>
-      <App />
-    </AnimatePresence>
-  </BrowserRouter>
+    <BrowserRouter>
+        <AnimatePresence>
+            <App />
+        </AnimatePresence>
+    </BrowserRouter>
 );
