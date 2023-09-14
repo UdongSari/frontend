83a9d70416a44796d9813b431793c17a2e74d237
toothlessdev <toothless042@gmail.com>
2023-09-15 02:48:28 +0900

    refactor: Request Detail 페이지 컴포넌트에 Profile 구현

--- src/pages/RequestDetail.jsx
+++ src/pages/RequestDetail.jsx
@@ -1,59 +1,20 @@
-import { React, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { List } from "../components/List";
+import { Profile } from "../components/Profile";
 import "./RequestDetail.scss";
-import { Button, InstaBtn, FacebookBtn } from "../components/Button";
-import Profile from "../components/Profile";
 
 export default function RequestDetail() {
-  const [photographer, setPhotographer] = useState("000 Portfolio ");
-  const [IdCard__text, setIdCard__text] = useState(
-    "strawberrymoon❤️❤️🍓❤️❤\nstrawberrymoon❤️❤️🍓❤️❤\nstrawberrymoon❤️❤️🍓❤️❤"
-  );
-
-  return (
-    <>
-      <div className="RequestDetail-wrapper">
-        <div className="IdCard">
-          <div className="IdCard__Profile">
-            <div className="IdCard__ProfileCircle">
-              <Profile />
-            </div>
-
-            <div className="IdCard__btns">
-              <Button
-                type="primary"
-                width="119px"
-                height="40px"
-                children={"수정하기"}
-              />
-              <Button type="insta" width="119px" height="40px" children={""} />
-              <InstaBtn />
-              <FacebookBtn />
-            </div>
-          </div>
-
-          <div className="IdCard-Container">
-            <span className="IdCard__title-Bold">{photographer}</span>
-            <div className="ListTable-container">
-              <List.Table />
-              <List.TableDetail />
-            </div>
-
-            <span className="IdCard__text">{IdCard__text} </span>
-          </div>
-          <div className="IdCard__slider">
-            <FontAwesomeIcon icon={faChevronUp} />
-          </div>
-          <div className="RequestDetail-container">
-            <div className="RequestDetail-grid">
-              <div className="RequestDetail-grid-activeitem" />
-              <div className="RequestDetail-grid-item" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <Profile.Wrapper>
+            <Profile.Aside imgSrc={""} igLink={"http://www.naver.com"} fbLink={"http://www.google.com"}></Profile.Aside>
+            <Profile.Content
+                title="My Profile"
+                rating={5}
+                theme={["테마", "긴 테마"]}
+                region="대구광역시 수성구 범어동"
+                time="1시간 ~ "
+                phone="010-1234-1234"
+                price="50,000원"
+                description="설명설명설명"
+            />
+        </Profile.Wrapper>
+    );
 }
