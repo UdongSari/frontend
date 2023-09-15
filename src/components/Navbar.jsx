fa70903d6438956249daab76dcba6eebcc321c4c
toothlessdev <toothless042@gmail.com>
2023-09-15 19:54:46 +0900

    feat: NavAside 컴포넌트 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,39 +4,59 @@ import "./NavBar.scss";
 import { Button } from "./Button";
 
 export const NavBar = ({ login }) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <nav className="nav-wrapper">
-      <ul className="nav-container">
-        <li>
-          <Link to="/">UdongSari</Link>
-        </li>
+    return (
+        <nav className="nav-wrapper">
+            <ul className="nav-container">
+                <li>
+                    <Link to="/">UdongSari</Link>
+                </li>
 
-        <li>
-          <Link to="/photo/response">찍어드려요</Link>
-        </li>
-        <li>
-          <Link to="/photo/request">찍어주세요</Link>
-        </li>
-        <li>
-          <Link to="/chat">채팅</Link>
-        </li>
+                <li>
+                    <Link to="/photo/response">찍어드려요</Link>
+                </li>
+                <li>
+                    <Link to="/photo/request">찍어주세요</Link>
+                </li>
+                <li>
+                    <Link to="/chat">채팅</Link>
+                </li>
 
-        <li style={{ flexGrow: "1" }} />
+                <li style={{ flexGrow: "1" }} />
 
-        <li>
-          <Button type="orange-filled" onClick={() => navigate("/auth/signin")}>
-            로그인
-          </Button>
-        </li>
+                <li>
+                    <Button type="orange-filled" onClick={() => navigate("/auth/signin")}>
+                        로그인
+                    </Button>
+                </li>
 
-        <li>
-          <Button type="orange-stroke" onClick={() => navigate("/auth/signup")}>
-            회원가입
-          </Button>
-        </li>
-      </ul>
-    </nav>
-  );
+                <li>
+                    <Button type="orange-stroke" onClick={() => navigate("/auth/signup")}>
+                        회원가입
+                    </Button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export const NavAside = {
+    Container: ({ children }) => {
+        return <aside className="nav-aside-container">{children}</aside>;
+    },
+    Item: ({ imgSrc, name, recentChat }) => {
+        return (
+            <div className="nav-aside-item">
+                <div className="nav-aside-item__img">
+                    <img src={imgSrc} alt="" />
+                </div>
+
+                <div className="nav-aside-item__text">
+                    <h3>{name}</h3>
+                    <p>{recentChat}</p>
+                </div>
+            </div>
+        );
+    },
 };
